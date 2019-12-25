99759cdcd54aa808fbbd06be50716edd617feb05
justbrentvanzant <justbrentvanzant@gmail.com>
2019-12-24 20:54:03 -0800

    yo plz write code in src folder not outside of it

--- src/routes/home.js
+++ src/routes/home.js
@@ -5,11 +5,10 @@ import MenuBar from "./navbar";
 //import $ from 'jquery';
 //import "../../assets/js/jquery.min.js";
 //import "../../assets/js/jquery.scrollex.min.js";
-import "../../assets/js/browser.min.js";
 //import "../../assets/js/breakpoints.min.js";
 //import "../../assets/js/util.js";
 //import "../../assets/js/main.js";
-import "../../assets/css/main.css";
+import "../assets/css/main.css";
 
 class Home extends React.Component {
   constructor(props) {
