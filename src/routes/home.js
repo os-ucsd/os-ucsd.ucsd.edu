9c63acb734530bc77a19e175907a632c5c0d8096
Bao <bghoang.ucsd.edu>
2019-12-25 00:16:26 -0800

    replace footer

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,14 +1,7 @@
 import React from "react";
-//import jquery from 'jquery';
 import MenuBar from "./navbar";
-//import Nav from "react-bootstrap/Nav";
-//import $ from 'jquery';
-//import "../../assets/js/jquery.min.js";
-//import "../../assets/js/jquery.scrollex.min.js";
-//import "../../assets/js/breakpoints.min.js";
-//import "../../assets/js/util.js";
-//import "../../assets/js/main.js";
 import "../assets/css/main.css";
+import Footer from "./footer";
 
 class Home extends React.Component {
   constructor(props) {
@@ -90,7 +83,7 @@ class Home extends React.Component {
             <div className="inner">
               <div className="content">
                 <h2>Learn about Open Source Software!</h2>
-                <p>Amet tincidunt arcu suspendisse consequat</p>
+                <p>We welcome everybody</p>
               </div>
               <ul className="actions stacked">
                 <li>
@@ -307,66 +300,8 @@ class Home extends React.Component {
               </ul>
             </div>
           </section>
-
-          <footer id="footer">
-            <div className="inner">
-              <ul className="icons">
-                <li>
-                  <a href="#" className="icon brands fa-twitter">
-                    <span className="label">Twitter</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="icon brands fa-facebook-f">
-                    <span className="label">Facebook</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="icon brands fa-instagram">
-                    <span className="label">Instagram</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="icon brands fa-github">
-                    <span className="label">GitHub</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="icon brands fa-linkedin-in">
-                    <span className="label">LinkedIn</span>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="icon solid fa-envelope">
-                    <span className="label">Envelope</span>
-                  </a>
-                </li>
-              </ul>
-              <ul className="contact">
-                <li>12345 Somewhere Road</li>
-                <li>Nashville, TN 00000</li>
-                <li>(000) 000-0000</li>
-              </ul>
-              <ul className="links">
-                <li>
-                  <a href="#">FAQ</a>
-                </li>
-                <li>
-                  <a href="#">Support</a>
-                </li>
-                <li>
-                  <a href="#">Terms</a>
-                </li>
-                <li>
-                  <a href="#">Contact</a>
-                </li>
-              </ul>
-              <p className="copyright">
-                &copy; Untitled. All rights reserved. Lorem ipsum dolor.
-              </p>
-            </div>
-          </footer>
         </div>
+        <Footer />
       </div>
     );
   }
