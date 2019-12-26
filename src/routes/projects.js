b0b7e6e0113f6ab1f78e1f762c5c993a5d640a56
Bao <bghoang.ucsd.edu>
2019-12-25 19:12:08 -0800

    add project page

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,11 +1,24 @@
 import React from "react";
 import MenuBar from "../components/navbar";
 import "../css/project.css";
+import axios from "axios";
+import { Card, Button } from "react-bootstrap";
 
 class Project extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      projects: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://api.github.com/search/repositories?q=topic:os-ucsd-project")
+      .then(res => {
+        this.setState({ projects: res.data });
+      })
+      .catch(err => console.log(err));
   }
 
   render() {
