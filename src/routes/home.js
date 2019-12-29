55151a1c1b61a7d426059deb2acc15cd0251797b
brendan <Brendan>
2019-12-28 21:38:46 -0800

    some home css

--- src/routes/home.js
+++ src/routes/home.js
@@ -6,6 +6,7 @@ import "../css/home.css";
 import getAllPRs from '../timeline/getTimelineData';
 import Timeline from '../timeline/Timeline';
 import Grid from '@material-ui/core/Grid';
+import Background from "../images/material-space/material-space4.jpeg";
 import UpcomingEvents from '../events/UpcomingEvents';
 import Button from '@material-ui/core/Button';
 import {Link} from 'react-router-dom';
@@ -87,7 +88,7 @@ class Home extends React.Component {
         <div className="is-preload">
 
           {/*Banner*/}
-          <section id="banner">
+          <section id="banner" style={{ backgroundImage: `url(${Background})`, height: "20px" }}>
             <div className="inner">
               <div className="content">
                 <h1>Open Source at UCSD</h1>
