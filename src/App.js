b64821f609fc47b699c45528c84d95de22a59c18
Brendan Lau <brendanwlau@gmail.com>
2020-09-05 19:10:38 -0700

    Added styles so that footer is at bottom of page and isn't fixed

--- src/App.js
+++ src/App.js
@@ -8,27 +8,26 @@ import Event from "./routes/event"
 import Contact from "./routes/contact"
 import Hacktoberfest from "./routes/hacktoberfest"
 import TutorialComponent from "./components/tutorial-component";
-
 import Footer from "./components/footer";
 import Header from "./components/header";
 
 function App() {
   return (
-    <div style={{ position: 'relative' }}>
+    <div style={{ position: 'relative', minHeight: '100vh' }}>
       <Header />
-
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/tutorials" component={Tutorial} />
-        <Route path="/tutorials/:tutorialID" component={TutorialComponent} />
-        <Route exact path="/projects" component={Project} />
-        <Route exact path="/events" component={Event} />
-        <Route exact path="/contact" component={Contact} />
-        <Route exact path="/hacktoberfest" component={Hacktoberfest} />
-      </Router>
-
-      <div style={{position: 'fixed', bottom: 0, width: '100%'}}>
+      <div style={{ paddingBottom: '8rem' }}>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/tutorials" component={Tutorial} />
+          <Route path="/tutorials/:tutorialID" component={TutorialComponent} />
+          <Route exact path="/projects" component={Project} />
+          <Route exact path="/events" component={Event} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/hacktoberfest" component={Hacktoberfest} />
+        </Router>
+      </div>
+      <div style={{position: 'absolute', bottom: 0, width: '100%'}}>
         <Footer />
       </div>
     </div>
