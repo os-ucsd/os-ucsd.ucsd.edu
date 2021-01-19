0cf7698b31d880aee89c65e3223829ec9f80faa2
blau0123 <brendanwlau@gmail.com>
2021-01-19 13:05:50 -0800

    Winter projects page created (#142)
    * Added winter projects page
    
    * Made new branch and PR for winter projects page

--- src/components/header.js
+++ src/components/header.js
@@ -65,9 +65,10 @@ class Header extends React.Component {
                             <a className="nav-item" href="/">Home</a>
                             <a className="nav-item" href="/about">About</a>
                             <a className="nav-item" href="/projects">Projects</a>
+                            <a className="nav-item" href="/winter/projects">Winter Projects</a>
                             <a className="nav-item" href="/tutorial">Tutorials</a>
                             <a className="nav-item" href="/events">Events</a>
-                            <a className="nav-item" href="/hacktoberfest">Hacktoberfest</a>
+                            {/*<a className="nav-item" href="/hacktoberfest">Hacktoberfest</a>*/}
                             <a className="nav-item" href="/contact">Contact</a>
                         </ul>
                     </nav>
